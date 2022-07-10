4d8c4167abf0cd5620b93bd8ec0cb5bec51c0220
Andrey <mastertixo.is@gmail.com>
2022-07-10 13:33:49 +0600

    Рейтинг со звездами и изменяемым значением

--- website/movies/templatetags/movie_tags.py
+++ website/movies/templatetags/movie_tags.py
@@ -1,13 +1,21 @@
 from django import template
 
-from movies.models import Category, Movie
+from movies.models import *
 
 register = template.Library()
+
 
 @register.simple_tag()
 def get_categories():
     """Возвращает список категорий"""
     return Category.objects.all()
+
+
+@register.simple_tag()
+def get_rating(movie):
+    """Возвращает рейтинг к фильму"""
+    print(Movie.objects.get(pk=movie.pk).rating_set.all()[0].star)
+    return Movie.objects.get(pk=movie.pk).rating_set.all()[0].star
 
 
 @register.inclusion_tag('movies/tags/last_movies.html')
